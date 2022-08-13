3e20011fd70f1d3d3560859a9955ed2974231810
R5on11c <candyclouds@tutanota.com>
2022-08-13 18:38:58 +0200

    Push initial state

---
+++ rules.mk
@@ -0,0 +1,7 @@
+BOOTLOADER = atmel-dfu # Elite-C Settings
+MOUSEKEY_ENABLE = no    # Mouse keys
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = yes
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
